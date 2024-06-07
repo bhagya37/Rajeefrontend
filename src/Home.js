5dd018f16f233ca6bd8a91511cc02dc88b2df798
bhagya37 <t.bhagyalaxmi3702@gamil.com>
2024-06-07 10:49:09 +0530

    webinar-frontend

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ function Home(){
 <div id="joinbtn">
  
  <a href="https://razorpay.me/@rajlee"> <button className="btn">JOIN NOW</button></a>
- <h1 className="footer">Rajalee Innovations Pvt Ltd </h1>
+ <h1 className="footer">Rajlee Innovations Pvt Ltd </h1>
  <img className="logo1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFJT_MijixgNFfAWm79DVJol1fshn1ls3bgaPNJBd_ZpPPShOCCzNJwPmLlK_vuSO0xuU&usqp=CAU" alt="title"/> 
  <Link to="/Login"><button className="adminlogin">Login</button></Link>
 </div>
